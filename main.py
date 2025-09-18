97b2d9921853ba6c7b1648c5109084403a861720
Felix Garcia Garcia <nxtfifi@gmail.com>
2025-09-18 11:48:00 -0600

    Agregamos el mensaje del calculo del circulo

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ def main():
     areas = Areas()
     print("Área de un triángulo con base 5 y altura 10:", areas.area_triangulo(5, 10))
     print("Área de un rectángulo con base 5 y altura 10:", areas.area_rectangulo(5, 10))
+    print("Área de un círculo con radio 7:", areas.area_circulo(7))
 if __name__ == "__main__":
     main()  
