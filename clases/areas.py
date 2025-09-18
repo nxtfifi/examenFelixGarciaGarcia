a76d0505c1d45181eb216fb8ad3738b75712a749
Felix Garcia Garcia <nxtfifi@gmail.com>
2025-09-18 11:38:50 -0600

    Agregamos el calculo de areas

---
+++ clases/areas.py
@@ -0,0 +1,10 @@
+class Areas:
+    def __init__(self):
+        pass
+    def area_triangulo(self, base, altura):
+        """Calcula el área de un triángulo dado la base y la altura."""
+        return (base * altura) / 2
+    def area_rectangulo(self, base, altura):
+        """Calcula el área de un rectángulo dado la base y la altura."""
+        return base * altura
+    
