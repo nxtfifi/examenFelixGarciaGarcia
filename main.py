3b7bd71c617933b2272e7c1ff9e0d24a2f57e451
Felix Garcia Garcia <nxtfifi@gmail.com>
2025-09-18 11:37:53 -0600

    Agregamos las pruebas a main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from clases.areas import Areas
+def main():
+    areas = Areas()
+    print("Área de un triángulo con base 5 y altura 10:", areas.area_triangulo(5, 10))
+    print("Área de un rectángulo con base 5 y altura 10:", areas.area_rectangulo(5, 10))
+if __name__ == "__main__":
+    main()  
